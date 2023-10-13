ca7b4744f065ceedd35fed0851b1d8c3e03fb5fd
omar <omarnabilsharnopu@gmail.cm>
2023-10-13 11:52:08 +0300

    app init

---
+++ lib/layout/home_layout.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class HomeLayout extends StatefulWidget {
+  static const String routeName = "HomeLayout";
+
+  @override
+  State<HomeLayout> createState() => _HomeLayoutState();
+}
+
+class _HomeLayoutState extends State<HomeLayout> {
+  int index = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(" To Do"),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        notchMargin: 8,
+        child: BottomNavigationBar(
+          currentIndex: index,
+          onTap: (value) {
+            index=value;
+            setState(() {});
+          },
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.list), label: ""),
+            BottomNavigationBarItem(icon: Icon(Icons.settings), label: ""),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+}
